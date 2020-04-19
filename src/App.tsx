d56549328caf707bdeffde695b26000baae8e744
aesher9o1 <aashiskumar986@gmail.com>
2020-04-19 10:31:45 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from './components/Typography/Heading'
+
+function App() {
+  return (
+    <div>
+      <p>Hey</p>
+      <Heading varients={1}>This is heading, One</Heading>
+    </div>
+  )
+}
+
+export default App
